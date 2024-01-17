ac0900a3a47beaedda5ee83737e98cd721844af4
gaeunlee95 <yuebn22@naver.com>
2024-01-17 10:05:02 +0900

    board

--- jwboot/src/main/java/com/khit/study/entity/BoardVO.java
+++ jwboot/src/main/java/com/khit/study/entity/BoardVO.java
@@ -2,14 +2,17 @@ package com.khit.study.entity;
 
 import java.util.Date;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@ToString
+@Setter
+@Getter
 public class BoardVO {
 	private int id;
 	private String title;
 	private String writer;
 	private String content;
 	private Date createdDate;
-	
 }
